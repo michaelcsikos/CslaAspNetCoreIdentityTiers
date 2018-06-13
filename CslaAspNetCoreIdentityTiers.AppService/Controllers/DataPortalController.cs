8d6c82b355a2564586508ddc95b5038bd2f5d798
Michael Csikos <michael@softbiz.com.au>
2018-06-13 13:18:40 +1000

    DataPortalController added

---
+++ CslaAspNetCoreIdentityTiers.AppService/Controllers/DataPortalController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Csla.Server.Hosts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CslaAspNetCoreIdentityTiers.AppService
+{
+    [Route("api/[controller]")]
+    public class DataPortalController
+        : HttpPortalController
+    {
+    }
+}
